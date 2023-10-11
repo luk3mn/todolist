77dca1b84082f1dc44f9e729c0ccd89abb93cd9e
luk3mn <lucasrenan2015@gmail.com>
2023-10-11 12:24:46 -0300

    feat: setup, database and validations

---
+++ src/main/java/br/com/luke/todolist/user/UserModel.java
@@ -0,0 +1,29 @@
+package br.com.luke.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data // Define os getters e setters no modulo
+@Entity(name = "tb_users")
+public class UserModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true) // forbid more than one value with the same name, in this case, "username"
+    private String username;
+    private String name;
+    private String password;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
